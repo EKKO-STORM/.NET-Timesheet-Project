96d26313474405d5102dc155a27a97a38b4d278c
EKKO-STORM <andrewxiego@gmail.com>
2017-11-02 11:03:22 +0000

    adding the project in

---
+++ MVC_project/MVC_project/Controllers/HappyController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_project.Models;
+
+namespace MVC_project.Controllers
+{
+    public class HappyController : Controller
+    {
+        //
+        // GET: /Happy/
+
+        public ActionResult abc()
+        {
+            Student alex = new Student();
+            alex.FirstName = "Alex";
+            alex.LastName = "stupid";
+            return View(alex);
+        }
+
+        public ActionResult Index()
+        {
+
+            return View();
+
+        }
+
+
+
+
+
+    }
+}
