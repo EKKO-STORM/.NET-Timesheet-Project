d5e6c53b898aa57856b597faa3aade7f41105775
EKKO-STORM <andrewxiego@gmail.com>
2017-11-07 10:17:14 +0000

    little change

---
+++ MVC_project/MVC_project/Controllers/BookForOtherController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_project.Controllers
+{
+    public class BookForOtherController : Controller
+    {
+        //
+        // GET: /BookForOther/
+
+        public ActionResult BookForOther()
+        {
+            return View();
+        }
+
+    }
+}
